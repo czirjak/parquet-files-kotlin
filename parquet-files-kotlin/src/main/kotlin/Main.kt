ff6f64b3b3ba0ec492be3489cd41778ba5242f51
Czirják Zoltán <czirjak.zoltan91@gmail.com>
2023-01-16 09:18:56 +0100

    hadoop

--- parquet-files-kotlin/src/main/kotlin/Main.kt
+++ parquet-files-kotlin/src/main/kotlin/Main.kt
@@ -1,18 +1,27 @@
-import org.apache.arrow.vector.VectorSchemaRoot
+import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.File
 
 fun main(args: Array<String>) {
     val inputFile = File(ClassLoader.getSystemResource("test.json")!!.toURI())
-
-
-    val parquetReader = ParquetReader()
-
-
-    parquetReader.read(inputFile)
-
-    val vectorSchemaRoot = VectorSchemaRoot.
-
     val outputFile = File(ClassLoader.getSystemResource("out.json")!!.toURI())
-    val parquetWriter = ParquetWriter()
-    parquetWriter.write(vectorSchemaRoot, outputFile)
+
+
+    val data = ObjectMapper().readValue<MyClass>(inputFile)
+
+    val schema = MessageTypeParser.parseMessageType(
+        "message MyClass {" +
+                "  required int32 id;" +
+                "  required binary name (UTF8);" +
+                "}"
+    )
+
+    val path = Path("path/to/file.parquet")
+    val writer = ParquetWriter.builder(path, GroupWriteSupport())
+        .withType(schema)
+        .build()
+
+    val group = data.toGroup(schema)
+    writer.write(group)
+
+    writer.close()
 }
